c3c7fe80766ae4429c070fd65841f44753038c0d
Ashwini B <ashwini02abp02@gmail.com>
2023-06-12 01:28:32 +0530

    433tx lib changed

--- Blind_Stick_Receiver.ino
+++ Blind_Stick_Receiver.ino
@@ -1,30 +1,49 @@
-#include <Gyver433.h>
+// ask_receiver.pde
+// -*- mode: C++ -*-
+// Simple example of how to use RadioHead to receive messages
+// with a simple ASK transmitter in a very simple way.
+// Implements a simplex (one-way) receiver with an Rx-B1 module
+// Tested on Arduino Mega, Duemilanova, Uno, Due, Teensy, ESP-12
 
-Gyver433_RX<2, 20> rx;
-Gyver433_TX<3> tx;
-char data[] = "ALERT";
+#include <RH_ASK.h>
+#ifdef RH_HAVE_HARDWARE_SPI
+#include <SPI.h> // Not actually used but needed to compile
+#endif
 
-void setup() {
-  pinMode(2, INPUT);
-  pinMode(3, OUTPUT);
-  pinMode(13, OUTPUT);
-  Serial.begin(9600);
-  attachInterrupt(0, isr, CHANGE);
+RH_ASK driver;
+// RH_ASK driver(2000, 4, 5, 0); // ESP8266 or ESP32: do not use pin 11 or 2
+// RH_ASK driver(2000, 3, 4, 0); // ATTiny, RX on D3 (pin 2 on attiny85) TX on D4 (pin 3 on attiny85), 
+// RH_ASK driver(2000, PD14, PD13, 0); STM32F4 Discovery: see tx and rx on Orange and Red LEDS
+
+void setup()
+{
+#ifdef RH_HAVE_SERIAL
+    Serial.begin(9600);	  // Debugging only
+#endif
+    if (!driver.init())
+#ifdef RH_HAVE_SERIAL
+         Serial.println("init failed");
+#else
+	;
+#endif
 }
 
-void isr() {
-  rx.tickISR();
-}
+void loop()
+{
+    uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+    uint8_t buflen = sizeof(buf);
 
+    if (driver.recv(buf, &buflen)) // Non-blocking
+    {
+	int i;
 
-void loop() {
-  if (rx.gotData()) {
-    char data[10];
-    Serial.println("Got data");
-    if (data == "ALERT") {
-      digitalWrite(13, HIGH);
-      delay(1000);
-      digitalWrite(13, LOW);
+	// Message with a good checksum received, dump it.
+	// driver.printBuffer("Got:", buf, buflen);
+      if(buflen == 5) {
+        Serial.println((char *)buf);
+      }else if (buflen == 4) {
+        float loc = *(float *) buf;
+        Serial.println(loc);
+      }
     }
-  }
 }
